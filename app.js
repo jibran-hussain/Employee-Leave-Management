d095b9c2d602baed17a0ec78efd18475fc42fb33
Jibran <jibran.hussain.mir@indxtechnology.com>
2023-12-15 01:00:29 +0530

    Added middlewares and routes

--- app.js
+++ app.js
@@ -5,15 +5,18 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 import 'dotenv/config';
-
+import cookieParser from 'cookie-parser';
 import employeeRoute from './src/routes/employee.js'
-
+import superadminRoute from './src/routes/superadmin.js'
+import adminRoute from './src/routes/admin.js'
 
 const app=express();
-
 const PORT=process.env.port || 8000
 
-
+app.use(cookieParser())
+app.use(express.json())
+app.use('/api',superadminRoute)
+app.use('/api',adminRoute)
 app.use('/api',employeeRoute)
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
