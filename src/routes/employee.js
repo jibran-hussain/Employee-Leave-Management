968618f062c39af210006991d92a122d065a9a21
Jibran <jibran.hussain.mir@indxtechnology.com>
2023-12-16 00:02:34 +0530

    Created controller methods deleteLeave and deleteEmployee

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -1,15 +1,17 @@
 import express from 'express'
 import { isAdmin } from '../middlewares/isAdmin';
-import {createEmployee,employeeSingin,applyForLeave,listAllLeaves,updateLeaves} from '../controllers/employee.js'
+import {createEmployee,employeeSingin,deleteEmployee,applyForLeave,listAllLeaves,updateLeaves, deleteLeave} from '../controllers/employee.js'
 import { isAuth } from '../middlewares/isAuth.js';
 import { isEmployee } from '../middlewares/isEmployee.js';
 const router=express.Router();
 
-router.post('/employees/signup',isAuth,isAdmin,createEmployee);
-router.post('/employees/signin',employeeSingin)
+router.post('/auth/employees/signup',isAuth,isAdmin,createEmployee);
+router.post('/auth/employees/signin',employeeSingin)
+router.delete('/employees/:employeeId',isAuth,isAdmin,deleteEmployee)
 router.post('/employees/leaves',isAuth,isEmployee,applyForLeave)
 router.get('/employees/:employeeId/leaves',isAuth,isEmployee,listAllLeaves);
 router.patch('/employees/:employeeId/leave/:leaveId',isAuth,isEmployee,updateLeaves)
+router.delete('/employees/:employeeId/leaves/:leaveId',isAuth,isEmployee,deleteLeave)
 
 
 export default router;  
