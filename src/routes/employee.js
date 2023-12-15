45c7c23215233a34f8c8509b995ba31f409f6f70
Jibran <jibran.hussain.mir@indxtechnology.com>
2023-12-15 14:35:09 +0530

    Added routes for applying for leave and to list all leaves

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import { isAdmin } from '../middlewares/isAdmin';
-import {createEmployee,employeeSingin} from '../controllers/employee.js'
+import {createEmployee,employeeSingin,applyForLeave,listAllLeaves} from '../controllers/employee.js'
+import { isAuth } from '../middlewares/isAuth.js';
+import { isEmployee } from '../middlewares/isEmployee.js';
 const router=express.Router();
 
-router.post('/employees/signup',isAdmin,createEmployee);
+router.post('/employees/signup',isAuth,isAdmin,createEmployee);
 router.post('/employees/signin',employeeSingin)
+router.post('/employees/leave',isAuth,isEmployee,applyForLeave)
+router.get('/employees/leaves',isAuth,isEmployee,listAllLeaves)
 
 
 export default router;  
