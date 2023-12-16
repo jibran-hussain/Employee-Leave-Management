c672d8b543519b24e4c7839fd7b6a35b73ffeb6d
Jibran <jibran.hussain.mir@indxtechnology.com>
2023-12-16 20:44:09 +0530

    Added logic for validating leave date

--- src/utils/validateLeave.js
+++ src/utils/validateLeave.js
@@ -1,12 +1,21 @@
-export const validateLeave=(date)=>{
-    try{
-        const [day,month,year]=date.split('-').map(Number);
-        const selectedDate=new Date(year,month,day);
-        const currentDate=new Date();
-        console.log(selectedDate,"here is the selected data")
-        if(selectedDate <= currentDate) return res.status(400).json({message:`You cannot apply leave on date from the past`});
-        if(selectedDate.getDay() == 5 || selectedDate.getDay() == 6) return res.status(400).json({message:`It's a weekend, already a holiday`})
-    }catch(e){
-        console.log(e)       
+export const validateLeave = (date) => {
+    try {
+        const [day, month, year] = date.split('-').map(Number);
+        const selectedDate = new Date(year, month - 1, day);
+        const currentDate = new Date();
+        if (selectedDate < currentDate) {
+            throw new Error(`You cannot apply for leave on a date from the past.`);
+        }
+
+        if (selectedDate.getDay() === 0 || selectedDate.getDay() === 6) {
+            throw new Error(`It's a weekend, already a holiday.`);
+        }
+        return true;
+    } catch (e) {
+        console.log(e)
+        throw(e); 
     }
-}
+};
+
+
+
