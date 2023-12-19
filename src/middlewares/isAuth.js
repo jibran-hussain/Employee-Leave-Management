26ed1c0b70c6e263b6429f13c7cc163cbad03c82
Jibran <jibran.hussain.mir@indxtechnology.com>
2023-12-19 13:40:33 +0530

    Removed console messages

--- src/middlewares/isAuth.js
+++ src/middlewares/isAuth.js
@@ -7,9 +7,8 @@ export const isAuth=async(req,res,next)=>{
         if(!jwtToken) return res.status(401).json({message:`Unauthorized (Token missing)`});
         const decodedToken=jwt.verify(jwtToken,process.env.JWT_SECRET_KEY);
         req.auth=decodedToken;
-        console.log(req.auth,`here is the docoded token`)
         next()
     }catch(e){
-        return res.status(401).json({message:`Invalid Token`})
+        return res.status(403).json({message:`Unauthorized. Access denied`})
     }
 }
