57dd78dc9f12f94a560f6567caa17d2cbd565f7b
Jibran <jibran.hussain.mir@indxtechnology.com>
2023-12-19 12:49:10 +0530

    Functions for checking validation of email and password before signup and signin

---
+++ src/utils/validations.js
@@ -0,0 +1,19 @@
+import validator from 'validator';
+
+export const isValidEmail=(email)=>{
+    try{
+        if(!validator.isEmail(email)) return false
+        return true;
+    }catch(e){
+        throw(e.message)
+    }
+}
+
+export const passwordValidation=(password)=>{
+    try{
+        if(validator.isEmpty(password,{ignore_whitespace:true})) return true;
+        return false;
+    }catch(e){
+        throw(e);
+    }
+}
