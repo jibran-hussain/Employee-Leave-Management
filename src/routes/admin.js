bd12c34a99020ce2f2641487f688c4df7cad1729
Jibran <jibran.hussain.mir@indxtechnology.com>
2023-12-20 16:33:20 +0530

    Added routes for applyLeave,updateLeave,listLeaves,delteLeave,deleteAdmin

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { createAdmin,adminSignin } from '../controllers/admin.js';
+import { createAdmin,adminSignin,applyForLeave,deleteAdmin,listAllLeaves,updateLeave } from '../controllers/admin.js';
 import { isSuperAdmin } from '../middlewares/isSuperAdmin.js';
-import { isAuth } from '../middlewares/isAuth.js';
+import { isAuth} from '../middlewares/isAuth.js';
+import { isAdmin } from '../middlewares/isAdmin.js';
+import { updateLeaves } from '../controllers/employee.js';
 const router=express.Router();
 
 router.post('/auth/admins/signup',isAuth,isSuperAdmin,createAdmin);
 router.post('/auth/admins/signin',adminSignin)
+router.delete('/admins/:adminId',isAuth,isSuperAdmin,deleteAdmin)
+router.post('/admins/leaves',isAuth,isAdmin,applyForLeave)
+router.get('/admins/leaves',isAuth,isAdmin,listAllLeaves)
+router.patch('/admins/leaves/:leaveId',isAuth,isAdmin,updateLeave)
 
 export default router;
