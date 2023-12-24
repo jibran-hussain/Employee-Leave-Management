520673af83284943aac4643041ed43c17a8a111a
Jibran <jibran.hussain.mir@indxtechnology.com>
2023-12-24 17:25:57 +0530

    converted date into UTC format

--- src/utils/getDate.js
+++ src/utils/getDate.js
@@ -1,6 +1,7 @@
 export const getDate=(date)=>{
-    console.log('here is the dateeeerrrrrrrrrre',typeof date,date)
-    const [day, month, year] = date.split('-').map(Number);
-    const selectedDate = new Date(year, month - 1, day);
+    
+    const [day, month, year] = date.split('-').map(date=>Number(date));
+    const selectedDate = new Date(Date.UTC(year, month-1, day,0,0,0));
+    if(selectedDate.getFullYear() != year || selectedDate.getMonth() != month-1 || selectedDate.getDate() != day) throw new Error('Invalid date(s)')
     return selectedDate;
 }
