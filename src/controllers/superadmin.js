72cff256a09e285392423f9f8dd421e1f20211e5
Jibran <jibran.hussain.mir@indxtechnology.com>
2023-12-24 17:46:11 +0530

    Remove auth controllers and added deleteAdmin method

--- src/controllers/superadmin.js
+++ src/controllers/superadmin.js
@@ -10,37 +10,24 @@ import { isValidEmail , passwordValidation} from '../utils/validations.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename)
 
-export const superadminSignin=async(req,res)=>{
+
+export const deleteAdmin=async (req,res)=>{
     try{
-        const {email,password,role}=req.body;
-        if(!email || !password || !role) return res.status(400).json({message:`All fields are necassary`})
-
-        // Check if it is a valid email or not
-        if(!isValidEmail(email)) return res.status(400).json({error:"Please enter valid email address"})
-
-        // Checks whether password is Empty
-        if(passwordValidation(password)) return res.status(400).json({error:`Password cannot be empty`})
-
-        // If the user is superadmin
-            if(role === "superadmin"){
-               const data= await fs.readFile(`${__dirname}/../../db/superadmin.json`,'utf8')
-               const fileData=JSON.parse(data);                    
-                    
-               if(email === fileData.superadmin.email && isValidPassword(password,fileData.superadmin.password)){
-
-               const token=jwt.sign({id:fileData.superadmin.superadminId,email:fileData.superadmin.email,role:"superadmin"},process.env.JWT_SECRET_KEY);
-               res.cookie('jwt',token,{
-                        httpOnly:true
-                    })
-               return res.json({
-                    token
-                })
-            }else{
-                return res.status(400).json({error:"Invalid Credentials. Please try again"})
-            }
-            }
+        const adminId=req.params.adminId
+        const data=await fs.readFile(`${__dirname}/../../db/admin.json`,'utf8')
+        const fileData=JSON.parse(data);
+        
+        const updatedAdminList= fileData.admins.filter((admin)=>{
+            if(admin.adminId == adminId) return false;
+            return admin;
+        })
+        fileData.admins=updatedAdminList;
+        const finalFileData=JSON.stringify(fileData);
+        await fs.writeFile(`${__dirname}/../../db/admin.json`,finalFileData,'utf8')
+        return res.json({message:"Admin Deleted Successfully"})
         
     }catch(e){
-       return res.json({error:e})
+        return res.json({error:e})
     }
 }
+
