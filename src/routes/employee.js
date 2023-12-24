134d024d796328dc246d8c940ef9a5656166a3a1
Jibran <jibran.hussain.mir@indxtechnology.com>
2023-12-24 17:38:02 +0530

    Added and deleted some routes

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -1,17 +1,18 @@
 import express from 'express'
 import { isAdmin } from '../middlewares/isAdmin.js';
-import {createEmployee,employeeSingin,deleteEmployee,applyForLeave,listAllLeaves,updateLeaves, deleteLeave} from '../controllers/employee.js'
+import {deleteEmployee,applyForLeave,listAllLeaves,updateLeaves, deleteLeave,getAllLeavesofEmployee} from '../controllers/employee.js'
 import { isAuth } from '../middlewares/isAuth.js';
 import { isEmployee } from '../middlewares/isEmployee.js';
+import { isAdminOrSuperadmin } from '../middlewares/isAdminOrSuperadmin.js';
 const router=express.Router();
 
-router.post('/auth/employees/signup',isAuth,isAdmin,createEmployee);
-router.post('/auth/employees/signin',employeeSingin)
-router.delete('/employees/:employeeId',isAuth,isAdmin,deleteEmployee)
 router.post('/employees/leaves',isAuth,isEmployee,applyForLeave)
-router.get('/employees/leaves',isAuth,isEmployee,listAllLeaves);
+router.get('/employees/leaves',isAuth,listAllLeaves);
 router.patch('/employees/leave/:leaveId',isAuth,isEmployee,updateLeaves)
 router.delete('/employees/leaves/:leaveId',isAuth,deleteLeave)
+router.delete('/employees/:employeeId',isAuth,isAdminOrSuperadmin,deleteEmployee)
+router.get('/employees/:employeeId/leaves',isAuth,isAdminOrSuperadmin,getAllLeavesofEmployee)   
 
 
-export default router;  
+
+export default router;
