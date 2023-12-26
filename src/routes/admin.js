a8915880448a57aad1608c8b035bfb41467089fd
Jibran <jibran.hussain.mir@indxtechnology.com>
2023-12-26 14:32:05 +0530

    Changed reference of the controller methods used in the routes

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,21 +1,21 @@
 import express from 'express';
-import { applyForLeave,deleteLeave,listAllLeaves,updateLeave,deleteAdmin,listAllAdmins } from '../controllers/admin.js';
+import { deleteLeave,deleteAdmin,listAllAdmins } from '../controllers/admin.js';
+import { applyForLeave,listAllAdminLeaves, updateLeave } from '../controllers/leave.js';
 import { isAuth} from '../middlewares/isAuth.js';
 import { isAdmin } from '../middlewares/isAdmin.js';
 import { isSuperAdmin } from '../middlewares/isSuperAdmin.js';
-import { isAdminOrSuperadmin } from '../middlewares/isAdminOrSuperadmin.js';
 const router=express.Router();
 
 // Routes related to admin and can be accessed by admin only.
 router.post('/admins/leaves',isAuth,isAdmin,applyForLeave)
 router.patch('/admins/leaves/:leaveId',isAuth,isAdmin,updateLeave)
 router.delete('/admins/leaves/:leaveId',isAuth,deleteLeave)
-router.get('/admins/leaves',isAuth,isAdmin,listAllLeaves)
+router.get('/admins/leaves',isAuth,isAdmin,listAllAdminLeaves)
 
 // Routes related to admin but can be accessed by superadmins oly
 router.get('/admins',isAuth,isSuperAdmin,listAllAdmins)
 router.delete('/admins/:adminId',isAuth,isSuperAdmin,deleteAdmin)
-router.get('/admins/:adminId/leaves',isAuth,isSuperAdmin,listAllLeaves)
+router.get('/admins/:adminId/leaves',isAuth,isSuperAdmin,listAllAdminLeaves)
 
 
 
