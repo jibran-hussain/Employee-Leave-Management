f0c34436a732d1fdbe5039db88cbca09d9242f41
Jibran <jibran.hussain.mir@indxtechnology.com>
2023-12-26 17:40:03 +0530

    Added the middleware to one route

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { deleteLeave,deleteAdmin,listAllAdmins } from '../controllers/admin.js';
-import { applyForLeave,listAllAdminLeaves, updateLeave } from '../controllers/leave.js';
+import { deleteAdmin,listAllAdmins } from '../controllers/admin.js';
+import { applyForLeave,listAllAdminLeaves, updateLeave ,deleteLeave } from '../controllers/leave.js';
 import { isAuth} from '../middlewares/isAuth.js';
 import { isAdmin } from '../middlewares/isAdmin.js';
 import { isSuperAdmin } from '../middlewares/isSuperAdmin.js';
@@ -9,7 +9,7 @@ const router=express.Router();
 // Routes related to admin and can be accessed by admin only.
 router.post('/admins/leaves',isAuth,isAdmin,applyForLeave)
 router.patch('/admins/leaves/:leaveId',isAuth,isAdmin,updateLeave)
-router.delete('/admins/leaves/:leaveId',isAuth,deleteLeave)
+router.delete('/admins/leaves/:leaveId',isAuth,isAdmin,deleteLeave)
 router.get('/admins/leaves',isAuth,isAdmin,listAllAdminLeaves)
 
 // Routes related to admin but can be accessed by superadmins oly
