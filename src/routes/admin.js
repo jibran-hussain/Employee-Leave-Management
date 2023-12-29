917580304bf3a1efe08a9d9d9f2d00868faa55f9
Jibran <jibran.hussain.mir@indxtechnology.com>
2023-12-29 16:41:12 +0530

    Refactored the code

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { deleteAdmin,listAllAdmins } from '../controllers/admin.js';
-import { applyForLeave,listAllAdminLeaves, updateLeave ,deleteLeave,listLeaves,updateLeaveByPutMethod } from '../controllers/leave.js';
+import { applyForLeave,listAllAdminLeaves, updateLeave ,deleteLeave,listLeaves,updateLeaveByPutMethod,getLeaveDetails } from '../controllers/leave.js';
 import { isAuth} from '../middlewares/isAuth.js';
 import { isAdmin } from '../middlewares/isAdmin.js';
 import { isSuperAdmin } from '../middlewares/isSuperAdmin.js';
@@ -12,6 +12,10 @@ router.patch('/admins/leaves/:leaveId',isAuth,isAdmin,updateLeave)
 router.put('/admins/leaves/:leaveId',isAuth,isAdmin,updateLeaveByPutMethod)
 router.delete('/admins/leaves/:leaveId',isAuth,isAdmin,deleteLeave)
 router.get('/admins/leaves',isAuth,listLeaves)
+router.get('admins/leaves/:leaveId',isAuth,getLeaveDetails)
+router.get('/admins/leaves/:leaveId',isAuth,isAdmin,getLeaveDetails)
+
+
 
 // Routes related to admin but can be accessed by superadmins oly
 router.get('/admins',isAuth,isSuperAdmin,listAllAdmins)
--- src/routes/employee.js
+++ src/routes/employee.js
@@ -1,25 +1,15 @@
 import express from 'express'
-import {deleteEmployee,listAllEmployees,listAllDisabledEmployees,activateAccount,getLoggedUsersDetails,updateProfile,getEmployeeDetails,updateEmployeeProfile,updatedProfileByPutMethod,updateEmployeeProfileByPut} from '../controllers/employee.js'
-import { applyForLeave,updateLeave,deleteLeave ,listAllEmployeeLeaves,listLeaves,updateLeaveByPutMethod} from '../controllers/leave.js';
+import {deleteEmployee,listAllEmployees,listAllDisabledEmployees,activateAccount,getLoggedUsersDetails,updateProfile,getEmployeeDetails,updateEmployeeProfile,updatedProfileByPutMethod,updateEmployeeProfileByPut,resetPassword} from '../controllers/employee.js'
 import { isAuth } from '../middlewares/isAuth.js';
-import { isEmployee } from '../middlewares/isEmployee.js';
 import { isAdminOrSuperadmin } from '../middlewares/isAdminOrSuperadmin.js';
 const router=express.Router();
 
-// Routes related to employee which only employee can access
-router.post('/employees/leaves',isAuth,isEmployee,applyForLeave)
-router.get('/employees/leaves',isAuth,listLeaves);
-router.patch('/employees/leave/:leaveId',isAuth,isEmployee,updateLeave)
-router.put('/employees/leave/:leaveId',isAuth,isEmployee,updateLeaveByPutMethod)
 
-router.delete('/employees/leaves/:leaveId',isAuth,isEmployee,deleteLeave)
 
-// Routes related to employee details which Admin and Superadmin can access
-router.get('/employees/:employeeId/leaves',isAuth,isAdminOrSuperadmin,listAllEmployeeLeaves)
 router.get('/employees',isAuth,isAdminOrSuperadmin,listAllEmployees)
+router.delete('/employees/:employeeId',isAuth,isAdminOrSuperadmin,deleteEmployee)
 router.get('/employees/disabled',isAuth,isAdminOrSuperadmin,listAllDisabledEmployees)
 router.post('/employee/:employeeId/activate',isAuth,isAdminOrSuperadmin,activateAccount)
-router.delete('/employees/:employeeId',isAuth,isAdminOrSuperadmin,deleteEmployee)
 router.get('/employees/:employeeId',isAuth,isAdminOrSuperadmin,getEmployeeDetails)
 router.patch("/employee/:employeeId",isAuth,isAdminOrSuperadmin,updateEmployeeProfile)
 router.put("/employee/:employeeId",isAuth,isAdminOrSuperadmin,updateEmployeeProfileByPut)
@@ -29,6 +19,7 @@ router.get('/me',isAuth,getLoggedUsersDetails)
 router.patch('/me',isAuth,updateProfile)
 router.put('/me',isAuth,updatedProfileByPutMethod)
 router.delete('/me',isAuth,deleteEmployee)
+router.patch('/me/password',isAuth,resetPassword)
 
 
 
