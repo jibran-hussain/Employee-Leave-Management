13ab90ee4f3a9ae0cfb21ff8792256878f726349
Jibran <jibran.hussain.mir@indxtechnology.com>
2023-12-29 10:54:04 +0530

    route for PUT method for updating his profile details

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { deleteAdmin,listAllAdmins } from '../controllers/admin.js';
-import { applyForLeave,listAllAdminLeaves, updateLeave ,deleteLeave } from '../controllers/leave.js';
+import { applyForLeave,listAllAdminLeaves, updateLeave ,deleteLeave,listLeaves,updateLeaveByPutMethod } from '../controllers/leave.js';
 import { isAuth} from '../middlewares/isAuth.js';
 import { isAdmin } from '../middlewares/isAdmin.js';
 import { isSuperAdmin } from '../middlewares/isSuperAdmin.js';
@@ -9,8 +9,9 @@ const router=express.Router();
 // Routes related to admin and can be accessed by admin only.
 router.post('/admins/leaves',isAuth,isAdmin,applyForLeave)
 router.patch('/admins/leaves/:leaveId',isAuth,isAdmin,updateLeave)
+router.put('/admins/leaves/:leaveId',isAuth,isAdmin,updateLeaveByPutMethod)
 router.delete('/admins/leaves/:leaveId',isAuth,isAdmin,deleteLeave)
-router.get('/admins/leaves',isAuth,isAdmin,listAllAdminLeaves)
+router.get('/admins/leaves',isAuth,listLeaves)
 
 // Routes related to admin but can be accessed by superadmins oly
 router.get('/admins',isAuth,isSuperAdmin,listAllAdmins)
