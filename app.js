7df96368d17b5cfeb9eadc97659d410eaa97fe1f
Jibran <jibran.hussain.mir@indxtechnology.com>
2023-12-29 16:54:30 +0530

    Added leaveRoute middleware

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 import express from 'express';
-import fs from 'fs';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 import 'dotenv/config';
 import authRoute from './src/routes/auth.js'
 import employeeRoute from './src/routes/employee.js'
 import adminRoute from './src/routes/admin.js'
+import leaveRoute from './src/routes/leaves.js'
 
 const app=express();
 const PORT=process.env.port || 8000
@@ -16,6 +12,8 @@ app.use(express.json())
 app.use(`/api/${process.env.API_VERSION}/auth`,authRoute)
 app.use(`/api/${process.env.API_VERSION}`,adminRoute)
 app.use(`/api/${process.env.API_VERSION}`,employeeRoute)
+app.use(`/api/${process.env.API_VERSION}`,leaveRoute)
+
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
 })
