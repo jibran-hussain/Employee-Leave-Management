1d07c7ee5d54518483925e507f37008a39febe7f
Jibran <jibran.hussain.mir@indxtechnology.com>
2023-12-31 16:30:00 +0530

    moved these files to other locations

--- src/utils/getDate.js
+++
@@ -1,7 +0,0 @@
-export const getDate=(date)=>{
-    
-    const [day, month, year] = date.split('-').map(date=>Number(date));
-    const selectedDate = new Date(Date.UTC(year, month-1, day,0,0,0));
-    if(selectedDate.getFullYear() != year || selectedDate.getMonth() != month-1 || selectedDate.getDate() != day) throw new Error('Invalid date(s)')
-    return selectedDate;
-}
--- src/utils/isValidDate.js
+++
@@ -1,4 +0,0 @@
-export const isValidDate=(date)=>{
-    if(date instanceof Date || date != 'Invalid Date') return false;
-    return true;
-}
