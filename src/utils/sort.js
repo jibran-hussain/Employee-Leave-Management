2ade5e806489b5e0ff4a677b1b748d214891a695
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-01-01 23:17:23 +0530

    Added functionality of sorting on the basis of date

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,5 +1,7 @@
+import { getDate } from "./Date/getDate.js";
+
 export const sort= (arr,sortBy,order)=>{
-    if (sortBy === 'name' || sortBy === 'role' || sortBy === 'salary' || sortBy === 'id') {
+    if (sortBy === 'name' || sortBy === 'role' || sortBy === 'salary' || sortBy === 'id' || sortBy === 'date') {
         arr.sort((a, b) => {
             let fieldA,fieldB;
             if(sortBy === 'name' || sortBy === 'role'){
@@ -11,6 +13,11 @@ export const sort= (arr,sortBy,order)=>{
                  fieldA = a[sortBy];
                  fieldB = b[sortBy];
                  return order === 1 ? fieldA - fieldB : fieldB - fieldA;
+            }
+            else if(sortBy =='date'){
+                fieldA = getDate(a[sortBy]);
+                fieldB = getDate(b[sortBy]);
+                return order === 1 ? fieldA.getTime() - fieldB.getTime() : fieldB.getTime() - fieldA.getTime();
             }
         });
     }
