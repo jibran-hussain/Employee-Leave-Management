01123ad6bad16bf8e36cd37f2663a4ca70d6e153
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-01-01 12:03:17 +0530

    modified code such that if the employee has already applied on this date,he will not not able to apply again on this date

--- src/utils/Date/getDatesArray.js
+++ src/utils/Date/getDatesArray.js
@@ -1,6 +1,6 @@
-import { getDate } from "./getDate.js"
+import { isAlreadyLeave } from "../leaves/isAlreadyLeave.js";
 
-export const getDatesArray=(fromDate,toDate)=>{
+export const getDatesArray= async (userId,fromDate,toDate)=>{
     try{
         let dates=[]
         let leaveDuration=0;
@@ -9,15 +9,15 @@ export const getDatesArray=(fromDate,toDate)=>{
         const endDate=toDate;
         const currentDate=new Date();
         currentDate.setUTCHours(0,0,0)
-        console.log(startDate,'startdate')
-        console.log(currentDate,'currentDate')
-        console.log(startDate <currentDate)
 
         while(startDate <= endDate){
-            if (startDate.getDay() === 0 || startDate.getDay() === 6 ){
+            const alreadyApplied= await isAlreadyLeave(userId,startDate);
+            
+            if (startDate.getDay() === 0 || startDate.getDay() === 6 || alreadyApplied ){
                 startDate.setDate(startDate.getDate()+1);
                 continue;
             }
+            
             dates.push(`${startDate.getDate() < 10?'0':''}${startDate.getDate()}-${startDate.getMonth()+1 < 10?'0':''}${startDate.getMonth()+1}-${startDate.getFullYear()}`);
             leaveDuration++;
             startDate.setDate(startDate.getDate()+1);
