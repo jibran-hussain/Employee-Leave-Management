3f9a0da60d1d0d77c7007f54743c930bedbe08e3
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-01-08 21:07:18 +0530

    Added email case validation in signin and signup and corrected some status codes

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,7 +1,6 @@
 import fs from 'fs/promises'
 import { fileURLToPath } from 'url';
 import {dirname} from 'path';
-import jwt from 'jsonwebtoken';
 import { isValidEmail,passwordValidation } from "../utils/Validation/validations.js";
 import { generateId } from '../utils/generateId.js';
 import { generateHashedPassword } from '../utils/Auth/generateHashedPassword.js';
@@ -34,43 +33,38 @@ export const createUser=async(req,res)=>{
         // Validatin salary
         if(!Number(salary)) return res.status(400).json("Please enter a valid salary")
 
-        // Fetching the data from users.json file
-        const data= await fs.readFile(`${__dirname}/../../db/users.json`,'utf8')
-        const fileData=JSON.parse(data);
-
-        // Check whether employee of this email-id already exists or not
-        const isExisting=fileData.users.some((user)=>user.email === email);
-        if(isExisting) return res.status(500).json({error:"Employee with this email already exists. Please try with another email id"})
-
-        // gnereate admin id 
-        let id= await generateId("user");
-
         if(role === 'admin'){
             if(req.auth.role != 'superadmin') return res.status(403).json({error:'You are not authorized to create an admin. Please login as superadmin'})
             const data=await fs.readFile(`${__dirname}/../../db/users.json`,'utf8')
             const fileData=JSON.parse(data);
 
             // Check whether admin of this email-id already exists or not
-            const isExisting=fileData.users.some((user)=>user.email === email);
-            if(isExisting) return res.status(500).json({error:"Admin with this email already exists. Please try with another email id"})
+            const isExisting=fileData.users.some((user)=>user.email === email.toLowerCase());
+            if(isExisting) return res.status(409).json({error:"Admin with this email already exists. Please try with another email id"})
+            
+            // gnereate admin id 
+            let id= await generateId("user");
 
             // Hashing the password
             const hashedPassword=generateHashedPassword(password);
 
-            const newAdmin={id,role:"admin",name,email,hashedPassword,mobileNumber,leavesLeft:20,leaveDetails:[],active:true};
+            const newAdmin={id,role:"admin",name,email:email.toLowerCase(),hashedPassword,mobileNumber,leavesLeft:20,leaveDetails:[],active:true};
 
             fileData.users.push(newAdmin);
             const newFileData=JSON.stringify(fileData)
             await fs.writeFile(`${__dirname}/../../db/users.json`,newFileData,'utf8')
-
-            // generate auth jwt token
-            const token=generateAuthToken(id,email,"admin")
 
             return res.status(201).json({message:`Admin created successfully`});
         }
         else if(role === "employee"){
             // Fetching the file and adding new employees
             if(req.auth.role != 'superadmin' &&  req.auth.role != 'admin') return res.status(403).json({error:'You are not authorized to create an employee. Please login as admin or superadmin'})
+            const data= await fs.readFile(`${__dirname}/../../db/users.json`,'utf8')
+            const fileData=JSON.parse(data);
+
+            // Check whether employee of this email-id already exists or not
+            const isExisting=fileData.users.some((user)=>user.email === email.toLowerCase());
+            if(isExisting) return res.status(409).json({error:"Employee with this email already exists. Please try with another email id"})
 
             // Get the employeeId for the new employee
             let id=await generateId("user");
@@ -78,20 +72,18 @@ export const createUser=async(req,res)=>{
             // Hashing the password
             const hashedPassword=generateHashedPassword(password);
             
-            const newEmployee={id,role:"employee",name,email,hashedPassword,mobileNumber,leavesLeft:20,leaveDetails:[],active:true,createdBy:req.auth.id};
+            const newEmployee={id,role:"employee",name,email:email.toLowerCase(),hashedPassword,mobileNumber,leavesLeft:20,leaveDetails:[],active:true,createdBy:req.auth.id};
             fileData.users.push(newEmployee);
             const newFileData=JSON.stringify(fileData)
 
             // Saving the new employee in JSON database
             await fs.writeFile(`${__dirname}/../../db/users.json`,newFileData,'utf8')
-            const token=generateAuthToken(id,email,"employee")
-            return res.json({message:`Employee created successfully`});
+            return res.status(201).json({message:`Employee created successfully`});
         }else{
-            return res.status(201).status(400).json({error:`Please enter a valid role`})
+            return res.status(400).json({error:`Please enter a valid role`})
         }
     }catch(e){
-        console.log(e)
-        return res.json({error:e.message})
+        return res.status(500).json({error:e.message})
     }
 }
 
@@ -115,7 +107,7 @@ export const userSignin=async(req,res)=>{
         let isDeactivated=false;
         
         const user=fileData.users.filter((user)=>{
-            if(user.email === email && isValidPassword(password,user.hashedPassword)){
+            if(user.email === email.toLowerCase() && isValidPassword(password,user.hashedPassword)){
                 if(user.active === false) isDeactivated=true;
                 return true;
             };
@@ -124,16 +116,15 @@ export const userSignin=async(req,res)=>{
 
         if(isDeactivated) return res.status(403).json({error:'Your account has been deactivated by the admin'})
          if(user.length > 0){
-                 const token=generateAuthToken(user[0].id,user[0].email,user[0].role)
+                 const token=generateAuthToken(user[0].id,user[0].email.toLowerCase(),user[0].role)
                  return res.json({
                          token
                     })
           }else{
-                return res.status(400).json({message:`Invalid credentials`})
+                return res.status(401).json({error:`Invalid credentials`})
             }
     }catch(e){
-        console.log(e)
-       return res.json({error:e.message})
+       return res.status(500).json({error:e.message})
     }
 }
 
