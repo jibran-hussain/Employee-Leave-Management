78dccde405c672d4218298a8ceb27a5d1cc838d3
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-01-16 23:57:37 +0530

    changed the error message

--- src/middlewares/isAdminOrSuperadmin.js
+++ src/middlewares/isAdminOrSuperadmin.js
@@ -3,7 +3,7 @@ import 'dotenv/config'
 export const isAdminOrSuperadmin=async(req,res,next)=>{
     try{
         const {role}=req.auth;
-        if(role != 'admin' && role!= 'superadmin') return res.status(401).json({message:"Unauthorized"})
+        if(role != 'admin' && role!= 'superadmin') return res.status(403).json({error:"Forbidden"})
         next()
     }catch(e){
         return res.status(401).json({error:e.message})
