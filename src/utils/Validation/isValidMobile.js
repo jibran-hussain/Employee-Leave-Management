dd854bcd59813c132b9b58d5c352be81adc6bcf4
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-01-16 23:56:30 +0530

    minor changes

--- src/utils/Validation/isValidMobile.js
+++ src/utils/Validation/isValidMobile.js
@@ -1,7 +1,6 @@
 // Checks if a mobile number is valid or not
 
 export const isValidNumber=(mobileNumber)=>{
-    if(!Number(mobileNumber)) throw new Error('Please enter a valid mobile Number')
-
-    if(mobileNumber.toString().length != 10) throw new Error('Mobile Number should be of 10 digits only')
+    if(!Number(mobileNumber) || mobileNumber.toString().length != 10) return false;
+    return true;
 }
