5c037d6d61c6b03f77d0ba3304b05295df549736
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-01-17 15:37:47 +0530

    Added some validation checks

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,16 +1,9 @@
-import fs from 'fs/promises'
-import { fileURLToPath } from 'url';
-import {dirname} from 'path';
 import { isValidEmail,passwordValidation } from "../utils/Validation/validations.js";
 import { generateHashedPassword } from '../utils/Auth/generateHashedPassword.js';
 import { generateAuthToken } from '../utils/Auth/geneateAuthToken.js';
 import { isValidPassword } from '../utils/Validation/isValidPassword.js'
 import { isValidNumber } from '../utils/Validation/isValidMobile.js';
 import Employee from '../models/employee.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename)
-
 
 // Creates an admin/employee wiht name,email,password,role,mobileNumber and salary as mandatory fileds.
 // Admin can only be created by Superadmin.
@@ -20,12 +13,14 @@ export const createUser=async(req,res)=>{
     try{
         const {name,email,password,role,mobileNumber,salary}=req.body;
 
-        if(!name ||!email || !password || !role || !mobileNumber ||!salary) return res.json({message:'All fields are mandatory'})
+        if(!name ||!email || !password || !role || !mobileNumber ||!salary) return res.status(400).json({error:'All fields are mandatory'})
+
+        if(name.length < 3) return res.status(400).json({error:'Name should be of atleast 3 characters'})
 
         if(role != 'admin' && role != 'employee') return res.status(400).json({error:`Please enter a valid role`})
 
         if(role === 'admin' && req.auth.role != 'superadmin') return res.status(403).json({error:'You are not authorized to create an admin. Please login as superadmin'});
-        if(role === "employee" && (req.auth.role != 'superadmin' ||  req.auth.role != 'admin')) return res.status(403).json({error:'You are not authorized to create an employee. Please login as admin or superadmin'})
+        if(role === "employee" && (req.auth.role != 'superadmin' &&  req.auth.role != 'admin')) return res.status(403).json({error:'You are not authorized to create an employee. Please login as admin or superadmin'})
 
         // Check if it is a valid email or not
         if(!isValidEmail(email)) return res.status(400).json({error:"Please enter valid email address"})
@@ -34,7 +29,7 @@ export const createUser=async(req,res)=>{
         if(passwordValidation(password)) return res.status(400).json({error:`Password cannot be empty and should have more than 3 characters`})
 
         // checks if a number is valid or not
-        isValidNumber(mobileNumber);
+        if(!isValidNumber(mobileNumber)) return res.status(400).json({error:`Please enter a valid mobile number`})
 
         // Validatin salary
         if(!Number(salary) && salary < 0) return res.status(400).json("Please enter a valid salary")
@@ -44,7 +39,7 @@ export const createUser=async(req,res)=>{
             email:email.toLowerCase()
         }})
 
-        if(isEmailExisting) return res.status(409).json({error:"Admin with this email already exists. Please try with another email id"})
+        if(isEmailExisting) return res.status(409).json({error:"Employee with this email already exists. Please try with another email id"})
             
         // Hashing the password
         const hashedPassword=generateHashedPassword(password);
@@ -73,6 +68,9 @@ export const userSignin=async(req,res)=>{
         if(passwordValidation(password)) return res.status(400).json({error:`Password cannot be empty and should have more than 3 characters`})
 
         const employee=await Employee.findOne({where:{email:email.toLowerCase()}})
+
+        // Check if account has been deactivated or not
+        if(employee.active == false) return res.status(403).json({error:`Your account has been deactived`});
         
         if(!employee) return res.status(401).json({error:`Invalid credentials`});
 
