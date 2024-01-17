2baf95e8d6a47e2f3c5284ed15ef245f51a262a5
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-01-17 15:34:41 +0530

    created Leave Model

---
+++ src/models/leaves.js
@@ -0,0 +1,20 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../../index.js';
+
+const Leave=sequelize.define('Leave',{
+    id:{
+        type: DataTypes.INTEGER,
+        autoIncrement:true,
+        primaryKey:true
+    },
+    reason:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    dates:{
+        type:DataTypes.ARRAY(DataTypes.DATEONLY),
+        allowNull:false
+    }
+})
+
+export default Leave;
