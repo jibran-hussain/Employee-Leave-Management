177ae3a2b7630c7091ed8f77faee445a2c8a2f25
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-01-19 15:37:33 +0530

    modified the error messages

--- src/middlewares/isAuth.js
+++ src/middlewares/isAuth.js
@@ -1,6 +1,3 @@
-import fs from 'fs/promises'
-import { fileURLToPath } from 'url';
-import {dirname} from 'path';
 import jwt from 'jsonwebtoken';
 import "dotenv/config"
 import Employee from '../models/employee.js';
@@ -14,11 +11,11 @@ export const isAuth=async(req,res,next)=>{
 
         // Check if the user has been deactivated or not
         const employee=await Employee.findByPk(id);
-        if(!employee) return res.status(403).json({error:'You are deactivated by the admin. You are not authorized to do anything.'})
+        if(!employee) return res.status(401).json({error:'Unauthorized'})
 
         req.auth=decodedToken;
         next()
     }catch(e){
-        return res.status(403).json({message:`Internal server error`})
+        return res.status(401).json({error:`Unauthorized`})
     }
 }
