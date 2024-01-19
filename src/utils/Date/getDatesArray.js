dd4de28fd8b3f4ff28b7863b1e051bd910348d2b
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-01-19 15:35:13 +0530

    made adjustments for updateLeave

--- src/utils/Date/getDatesArray.js
+++ src/utils/Date/getDatesArray.js
@@ -2,7 +2,7 @@ import { isAlreadyLeave } from "../leaves/isAlreadyLeave.js";
 
 // Genrates the dates between the range of dates for applying leave excluding the day if it is a weekend or on this day user has already applied for leave
 
-export const getDatesArray= async (userId,fromDate,toDate,considerAlreadyApplied)=>{
+export const getDatesArray= async (userId,fromDate,toDate,leaveConsider,leaveId)=>{
     try{
         let dates=[]
         let leaveDuration=0;
@@ -14,7 +14,8 @@ export const getDatesArray= async (userId,fromDate,toDate,considerAlreadyApplied
 
         while(startDate <= endDate){
             let alreadyApplied=false;
-            if(considerAlreadyApplied) alreadyApplied= await isAlreadyLeave(userId,startDate);
+            if(leaveConsider) alreadyApplied= await isAlreadyLeave(userId,startDate,leaveConsider);
+            else alreadyApplied= await isAlreadyLeave(userId,startDate,leaveConsider,leaveId);
             
             if (startDate.getDay() === 0 || startDate.getDay() === 6 || alreadyApplied){
                 startDate.setDate(startDate.getDate()+1);
