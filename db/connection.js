a09d5b2918a4cfab102c5d66775913223d9f5823
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-01-19 15:44:50 +0530

    introduced has to many association between Employees and Leaves

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,15 @@
 import sequelize from '../index.js'
 import Employee from "../src/models/employee.js";
+import Leave from '../src/models/leaves.js';
 
 export const connectToDB=async()=>{
     try{
         await sequelize.authenticate();
+        // has to many association
+        Employee.hasMany(Leave,{foreignKey:"employeeId"})
+        Leave.belongsTo(Employee,{foreignKey:"employeeId"})
         await Employee.sync();
+        await Leave.sync();
         console.log('Connection has been established successfully.');
       } catch (error) {
         console.error('Unable to connect to the database:', error);
