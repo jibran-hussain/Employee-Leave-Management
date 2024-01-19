0eee4e55083b82126d2f792c3c6acfdbe59008b2
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-01-19 15:45:34 +0530

    deleted admin routes imports

--- app.js
+++ app.js
@@ -2,17 +2,13 @@ import express from 'express';
 import 'dotenv/config';
 import authRoute from './src/routes/auth.js'
 import employeeRoute from './src/routes/employee.js'
-import adminRoute from './src/routes/admin.js'
 import leaveRoute from './src/routes/leaves.js'
 import {connectToDB} from './db/connection.js'
-import Employee from './src/models/employee.js';
-
 const app=express();
 const PORT=process.env.port || 8000
 
 app.use(express.json())
 app.use(`/api/${process.env.API_VERSION}/auth`,authRoute)
-app.use(`/api/${process.env.API_VERSION}`,adminRoute)
 app.use(`/api/${process.env.API_VERSION}`,employeeRoute)
 app.use(`/api/${process.env.API_VERSION}`,leaveRoute)
 
