1167712f98470d1928f3d7e6f4b9fd54de792c13
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-01-25 12:11:59 +0530

    replaced hard core values with environment variables

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { Sequelize } from "sequelize";
+import 'dotenv/config'
 
-const sequelize=new Sequelize('employee_leave_management_system', 'postgres', 'postgres',{
+const sequelize=new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD,{
     host:'localhost',
     dialect:'postgres'
 })
