605a4c0dd475b411fa9261b053668c916ac61777
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-01-25 12:10:24 +0530

    made connectToDB a middleware

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ import sequelize from '../index.js'
 import Employee from "../src/models/employee.js";
 import Leave from '../src/models/leaves.js';
 
-export const connectToDB=async()=>{
+export const connectToDB=async(req,res,next)=>{
     try{
         await sequelize.authenticate();
         // has to many association
@@ -11,7 +11,8 @@ export const connectToDB=async()=>{
         await Employee.sync();
         await Leave.sync();
         console.log('Connection has been established successfully.');
+        next()
       } catch (error) {
-        console.error('Unable to connect to the database:', error);
+        return res.status(500).json({error:`Internal Server Error`});
       }
 }
