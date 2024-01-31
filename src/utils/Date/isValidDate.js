d044c0d6c5da93226834dee052f80e02984418cb
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-01-31 11:01:34 +0530

    moved these files to Backend folder

--- src/utils/Date/isValidDate.js
+++
@@ -1,6 +0,0 @@
-// checks if the date is valid or not
-
-export const isValidDate=(date)=>{
-    if(date instanceof Date || date != 'Invalid Date') return false;
-    return true;
-}
--- src/utils/Validation/isValidMobile.js
+++
@@ -1,6 +0,0 @@
-// Checks if a mobile number is valid or not
-
-export const isValidNumber=(mobileNumber)=>{
-    if(!Number(mobileNumber) || mobileNumber.toString().length != 10) return false;
-    return true;
-}
