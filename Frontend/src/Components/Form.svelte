4ac0b93dc38029da932fbd3245bf3eb2bc2784fb
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-02-01 23:39:11 +0530

    created a dynamic Form component, Navbar, Sidebar and Table components

---
+++ Frontend/src/Components/Form.svelte
@@ -0,0 +1,67 @@
+<script>
+export let options;
+
+export let handleSubmit
+let formData={};
+export let error;
+console.log('in form')
+</script>
+{JSON.stringify({formData})}<br>
+
+<div class="container mt-5 w-25">
+    <h2 class="mb-5">Signin</h2>
+    {#if error}
+        <p class="text-center text-danger">{error}</p>
+    {/if}
+
+    <form class="form" on:submit|preventDefault={()=>handleSubmit(formData)}>
+        
+        {#each options as option(option)}
+            {#if option.type === 'text'}
+
+                <div class="mb-3 mt-3">
+                    <label for={option.name} class="form-label">{option.label}:</label>
+                    <input type="text" class="form-control" bind:value={formData[option.name]} id={option.name} placeholder={option.placeholder} name={option.name}>
+                </div>
+    
+            {:else if option.type === 'password'}
+
+                <div class="mb-3">
+                    <label for={option.name} class="form-label">{option.label}</label>
+                    <input type="password" class="form-control" bind:value={formData[option.name]} id={option.name} placeholder={option.placeholder}>
+                </div>
+    
+            {:else if option.type === 'email'}
+
+            <div class="mb-3 mt-3">
+                <label for={option.name} class="form-label">{option.label}</label>
+                <input type="email" class="form-control" bind:value={formData[option.name]} id={option.name} placeholder={option.placeholder} name={option.name}>
+              </div>
+    
+            <!-- {:else if option.type === 'date'}
+            <div class="form-input">
+                <label>{option.label}
+                    <br>
+                    <input type='date' placeholder={option.placeholder} bind:value={formData[option.name]} />
+                </label><br>
+            </div>
+            
+            {:else if option.type === 'select' }
+                <div class="form-input">
+                    <label>{option.label}
+                        <br>
+                        <select bind:value={formData[option.name]}>{option.label}
+                            <option selected disabled>--select--</option>
+                            {#each option.options as option}
+                                <option value={option.value}>{option.value}</option>
+                            {/each}
+                        </select>
+                    </label><br>
+                </div> -->
+            {/if}
+        {/each}
+    
+        <button type="submit" class="btn btn-primary w-100 mt-3">Submit</button>
+
+    </form>
+</div>
---
+++ Frontend/src/Components/Sidebar.svelte
@@ -0,0 +1,42 @@
+<script>
+    
+    let employee = {
+      name: "John Doe",
+      designation: "Software Engineer",
+      avatar: "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&q=70&fm=webp",
+    };
+  
+    let menuItems = [
+      { label: "All Employees", action: "listAllEmployees" },
+      { label: "Get All Leaves", action: "getAllLeaves" },
+      { label: "List Personal Leaves", action: "listPersonalLeaves" },
+      { label: "Reset Password", action: "resetPassword" },
+    ];
+
+    
+    function handleMenuClick(action) {
+      // Handle menu item click based on the action
+      console.log(`Clicked on ${action}`);
+    }
+
+  </script>
+  
+
+  <nav id="sidebar" class="col-md-3 col-lg-2 d-md-blocksidebar">
+    <div class="text-center">
+      <img src={employee.avatar} alt="Profile Picture" class="img-fluid rounded-circle mt-3 mb-2" width="100" height="100" />
+      <h5 class="mb-0">{employee.name}</h5>
+      <p class="text-muted">{employee.designation}</p>
+    </div>
+
+    <hr />
+
+    <ul class="nav flex-column">
+      {#each menuItems as { label, action }}
+        <li class="nav-item">
+          <a href="#" on:click={() => handleMenuClick(action)} class="nav-link text-dark">{label}</a>
+        </li>
+      {/each}
+    </ul>
+  </nav>
+  
