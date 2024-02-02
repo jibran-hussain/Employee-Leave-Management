05079c49794a29f52ba9b2a2808eb5dc1aeaa421
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-02-02 23:55:30 +0530

    contains views of different routes

---
+++ Frontend/src/views/AdminDashboard.svelte
@@ -0,0 +1,332 @@
+<script>
+    import { onMount } from "svelte";
+    import Navbar from "../Components/Navbar.svelte";
+    import Sidebar from "../Components/Sidebar.svelte";
+    import { user } from "../stores/userStore";
+    import toast, { Toaster } from 'svelte-french-toast';
+    import UpdateEmployeeModal from "../Components/UpdateEmployeeModal.svelte";
+
+
+    let employeesListData;
+    let showDeletedEmployees=false;
+    let searchInput=''
+    let selectedOption=''
+    let showUpdateModal=false;
+    let userToUpdate;
+
+    const openUpdateModel=()=>{
+        showUpdateModal=true;
+    }
+
+
+
+    $:{
+        console.log(selectedOption,'here is the selected Option')
+    }
+
+    const {id,email,role,token}=$user;
+    console.log(`here is ${id},${email} and ${role}`)
+
+    $: {
+        if (showDeletedEmployees) {
+            fetchDeletedEmployees().then(data => {
+                employeesListData = data;
+            });
+        } else {
+            fetchActiveEmployees().then(data => {
+                employeesListData = data;
+            });
+        }
+}
+
+
+
+    const fetchActiveEmployees=async()=>{
+        try{
+            const response=await fetch(`http://localhost:3000/api/v1/employees`,{
+                headers:{
+                    'Authorization':`Bearer ${token}`
+                }
+            })
+            const data=await response.json();
+            return data;
+        }catch(e){
+            console.log(`Here is the error`, e.message)
+        }
+    }
+
+
+    const fetchDeletedEmployees=async()=>{
+        try{
+            const response=await fetch(`http://localhost:3000/api/v1/employees?deleted=true`,{
+                headers:{
+                    'Authorization':`Bearer ${token}`
+                }
+            })
+            const data=await response.json();
+            return data;
+        }catch(e){
+            console.log(`Here is the error`, e.message)
+        }
+    }
+
+    const handleSearch=async ()=>{
+        try{
+            
+            let url=`http://localhost:3000/api/v1/employees?search=${searchInput}`;
+            if(showDeletedEmployees) url += `&deleted=true`
+            if(selectedOption) url += `&sortBy=${selectedOption}`
+            else url=url
+            const response=await fetch(url,{
+                headers:{
+                    'Authorization':`Bearer ${token}`
+                }
+            })
+            const data=await response.json();
+            if(data.message) employeesListData='';
+            else employeesListData = data;
+            
+
+            console.log(data,'seeeeeeeeeeeeeeeeeaaaaaaaaaaaaaaaaaaaarrrrrrrrrccccccccchhhhhhhhhhhhhhhh')
+        }catch(error){
+            console.log(error.message)
+        }
+    }
+
+    const handleSortBy=async()=>{
+        try{
+            console.log('select changedd')
+            let url=`http://localhost:3000/api/v1/employees?sortBy=${selectedOption}`;
+            if(showDeletedEmployees) url += `&deleted=true`
+            if(searchInput) url += `&search=${searchInput}`
+            const response=await fetch(url,{
+                headers:{
+                    'Authorization':`Bearer ${token}`
+                }
+            })
+            const data=await response.json();
+            if(data.message) employeesListData='';
+            else employeesListData = data;
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+const handleDeleteEmployee=async(employeeId)=>{
+    try{
+        const response=await fetch(`http://localhost:3000/api/v1/employees/${employeeId}`,{
+                method:'DELETE',
+                headers:{
+                    'Authorization':`Bearer ${token}`
+                }
+            })
+
+            if(response.ok){
+                toast.success('Employee deleted successfully', {
+                    duration: 5000,
+                    position: 'top-center', // Set the position to 'top'
+                });
+            employeesListData=await fetchActiveEmployees();
+            }else{
+                toast.error('You are not authorized to delete this employee',{
+                    duration:3000
+                });
+            }
+    }catch(error){
+        console.log(error.message)
+    }
+}
+
+const handleUpdateEmployee=async(employeeId)=>{
+    try{
+        const response=await fetch(`http://localhost:3000/api/v1/employees/${employeeId}`,{
+            headers:{
+                'Authorization':`Bearer ${$user.token}`
+            }
+        })
+        const data=await response.json();
+        userToUpdate=data.data;
+        showUpdateModal=true;
+        console.log(data,'here is the data from update button')
+    }catch(e){
+        console.log(e.message)
+    }
+}
+
+const handleActivateEmployee=async(employeeId)=>{
+    try{
+        const response=await fetch(`http://localhost:3000/api/v1/employees/${employeeId}/activate`,{
+                method:'POST',
+                headers:{
+                    'Authorization':`Bearer ${token}`
+                }
+            })
+
+            if(response.ok){
+                toast.success('Employee Activated successfully', {
+                    duration: 5000,
+                    position: 'top-center', // Set the position to 'top'
+                });
+            employeesListData=await fetchDeletedEmployees();
+            }else{
+                toast.error('You are not authorized to delete this employee',{
+                    duration:3000
+                });
+            }
+    }catch(error){
+
+    }
+}
+
+    onMount(async()=>{
+        employeesListData=await fetchActiveEmployees();
+    })
+
+    const handlePageChange=async(event,offset)=>{
+        try{
+            console.log(searchInput,'Here is the search input')
+            let url=`http://localhost:3000/api/v1/employees?offset=${offset}`;
+
+            if (searchInput.trim() !== '') {
+            url += `&search=${encodeURIComponent(searchInput.trim())}`;
+        }
+
+        // Check if there is a sort option selected
+        if (selectedOption.trim() !== '') {
+            url += `&sortBy=${encodeURIComponent(selectedOption.trim())}`;
+        }
+            const response=await fetch(url,{
+                headers:{
+                    'Authorization':`Bearer ${token}`
+                }
+            })
+            const data=await response.json();
+            employeesListData=data;
+        }catch(error){
+            console.log(`Here is the error in page change`, e.message)
+        }
+    }
+
+</script>
+<Toaster />
+
+{#if showUpdateModal}
+ <UpdateEmployeeModal {userToUpdate} on:modalClosed={()=>showUpdateModal=false} />
+{/if}
+
+<Navbar />
+<div class="container-fluid">
+    <div class="row vh-100">
+    <!-- Sidebar -->
+    <Sidebar />
+
+    <!-- Main content -->
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4 bg-light">
+            <!-- <h1>Display Area</h1> -->
+        
+        <div class="chartjs-size-monitor pt-4">
+            <!-- Searching and sorting -->
+            <div class="mb-3">
+                <div class="row  align-items-center justify-content-end">
+                    <div class="col-4 d-flex  justify-content-center">
+                        <input type="text" id="searchInput" class="form-control input-lg" bind:value={searchInput} on:input={()=>searchInput && handleSearch()} placeholder="Search" style="height: 50%;">
+                    </div>
+                    <div class="col-4 d-flex  justify-content-center align-items-center">
+                        <label for="sortBySelect" class="col-form-label"><span style="white-space: nowrap; padding-right:10px">Sort By</span></label>
+                        <select id="sortBySelect" class="form-select custom-select" bind:value={selectedOption} on:change={handleSortBy} style="height: 50%;">
+                            <option selected>Choose...</option>
+                            <option value="id">Id</option>
+                            <option value="name">Name</option>
+                            <option value="salary">Salary</option>
+                            <option value="mobileNumber">Mobile Number</option>
+                            <option value="role">Role</option>
+                        </select>
+                    </div>
+                    <div class="col-4">
+                        <div class="form-check d-flex  justify-content-center">
+                            <input class="form-check-input" type="checkbox" id="deletedCheckbox" bind:checked={showDeletedEmployees}>
+                            <label class="form-check-label" for="deletedCheckbox">
+                                Deleted
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            {#if employeesListData}
+    
+                <div class="table-responsive">
+                    <table class="table table-bordered">
+                        <thead class="text-center">
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Mobile Number</th>
+                            <th scope="col">Salary</th>
+                            <th scope="col">Leaves Left</th>
+                            <th scope="col">Role</th>
+                            <th scope="col"></th>
+                            <th scope="col"></th>
+                        </tr>
+                        </thead>
+                        <tbody class="text-center">
+                        {#each employeesListData.data as employee(employee)}
+                            <tr>
+                                <td class="align-middle">{employee.id}</td>
+                                <td class="align-middle">{employee.name}</td>
+                                <td class="align-middle">{employee.email}</td>
+                                <td class="align-middle">{employee.mobileNumber}</td>
+                                <td class="align-middle">{employee.salary}</td>
+                                <td class="align-middle" >{employee.leavesLeft}</td>
+                                <td class="align-middle">{employee.role}</td>
+                                <td class="align-middle">
+                                    {#if showDeletedEmployees}
+                                    <button type="button" class="btn btn-success" on:click={()=>handleActivateEmployee(employee.id)}>Activate</button>
+                                    {:else}
+                                        <button type="button" class="btn btn-danger" on:click={()=>handleDeleteEmployee(employee.id)}>Delete</button>
+                                    {/if}
+                                </td>
+                                <td class="align-middle"><button type="button" class="btn btn-primary" on:click={()=>handleUpdateEmployee(employee.id)}>Update</button></td>
+
+                            </tr>
+                        {/each}
+                        </tbody>
+                    </table>
+                </div>
+                
+                {:else}
+                    <h1>The List is empty</h1>
+            {/if}
+
+
+            <!-- Pagination -->
+
+            {#if employeesListData}
+            <nav aria-label="..." class="d-flex justify-content-center align-items-center">
+                <ul class="pagination">
+                <li class="page-item disabled">
+                    <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                </li>
+                
+                {#each Array.from({ length: employeesListData.metadata.totalPages }) as _, i (i+1)}
+                    {#if employeesListData.metadata.currentPage === i+1}
+                    <li class="page-item active" aria-current="page">
+                        <a class="page-link" href="#" on:click|preventDefault={(event)=>handlePageChange(event,i+1)}>{i+1}</a>
+                    </li>
+                    {:else}
+                    <li class="page-item"><a class="page-link" href="#" on:click|preventDefault={(event)=>handlePageChange(event,i+1)}>{i+1}</a></li>
+                    {/if}
+                {/each}
+                
+                <li class="page-item">
+                    <a class="page-link" href="#">Next</a>
+                </li>
+                </ul>
+            </nav>
+            
+            {/if}
+        </div>
+    </main>
+    </div>
+</div>
---
+++ Frontend/src/views/Signin.svelte
@@ -0,0 +1,70 @@
+<script>
+    import '../global.css';
+    import { navigate } from "svelte-routing";
+    import Form from '../Components/Form.svelte';
+    import {decodeJwtToken} from '../utils/decodeJwtToken.js'
+    import {user} from '../stores/userStore.js'
+
+    const formFields=[
+               {type:'email',name:'email',label:'Email',placeholder:'Enter email'},
+               {type:'password',name:'password',label:'Password',placeholder:'Password'},
+            ]
+
+    let error=''
+    let isError=false;
+    let data;
+
+    const handleSubmit=async(formData)=>{
+        try{
+            const response = await fetch(`http://localhost:3000/api/v1/auth/signin`, {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+            });
+             data=await response.json()
+            // show the error
+            if(data.error){
+                isError=true;
+            }
+            else{
+                const {token}=data;
+                localStorage.setItem('jwt',`${JSON.stringify(token)}`)
+                const decodedToken=decodeJwtToken(token);
+                user.set(decodedToken)
+                navigate("/dashboard", { replace: true });
+                console.log($user,'just checking')
+            }
+            
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    $:{
+        if(isError) error=data.error
+    }
+    console.log("in parent")
+
+</script>
+
+<header>
+    <p class="mb-0">EMPLOYEE LEAVE MANAGEMENT SYSTEM</p>
+</header>
+
+<Form options={formFields} formHeading="Signin" {handleSubmit} {error} />
+
+<style>
+    header{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-image: linear-gradient(to left, #553c9a, #b393d3);
+        color: white;
+        font-weight:600;
+        height: 7vh;
+        margin-bottom: 7em;
+    }
+</style>
