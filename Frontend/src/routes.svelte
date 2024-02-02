e2478db81a1ab6559c8e3c2462aa1f3096250231
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-02-02 23:58:29 +0530

    contains info about which view to dispaly on which route

---
+++ Frontend/src/routes.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { Router, Link, Route } from "svelte-routing";
+    import Signin from './views/Signin.svelte'
+    import AdminDashboard from "./views/AdminDashboard.svelte";
+    import Test from "./views/Test.svelte";
+</script>
+
+<Router>
+
+      <Route path="/">
+        <Signin />
+      </Route>
+
+      <Route path="/dashboard">
+        <AdminDashboard />
+      </Route>
+    
+</Router>
