3acf503d20b5126aa02ae198fdf634590b22f646
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-02-03 23:02:00 +0530

    created employee list table with an option to delete,activate and update them

---
+++ Frontend/src/Components/EmployeeListTable.svelte
@@ -0,0 +1,49 @@
+<!-- EmployeeTable.svelte -->
+
+<script>
+    export let employeesListData;
+    export let showDeletedEmployees;
+    export let handleActivateEmployee;
+    export let handleDeleteEmployee;
+    export let handleUpdateEmployee;
+  </script>
+  
+  <div class="table-responsive">
+    <table class="table table-bordered">
+      <thead class="text-center">
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">Mobile Number</th>
+          <th scope="col">Salary</th>
+          <th scope="col">Leaves Left</th>
+          <th scope="col">Role</th>
+          <th scope="col"></th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody class="text-center">
+        {#each employeesListData.data as employee (employee.id)}
+          <tr>
+            <td class="align-middle">{employee.id}</td>
+            <td class="align-middle">{employee.name}</td>
+            <td class="align-middle">{employee.email}</td>
+            <td class="align-middle">{employee.mobileNumber}</td>
+            <td class="align-middle">{employee.salary}</td>
+            <td class="align-middle">{employee.leavesLeft}</td>
+            <td class="align-middle">{employee.role}</td>
+            <td class="align-middle">
+              {#if showDeletedEmployees}
+                <button type="button" class="btn btn-success" on:click={() => handleActivateEmployee(employee.id)}>Activate</button>
+              {:else}
+                <button type="button" class="btn btn-danger" on:click={() => handleDeleteEmployee(employee.id)}>Delete</button>
+              {/if}
+            </td>
+            <td class="align-middle"><button type="button" class="btn btn-primary" on:click={() => handleUpdateEmployee(employee.id)}>Update</button></td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
+  
