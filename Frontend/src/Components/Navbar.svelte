2e4050de73491ac043671620127d814f8523f85a
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-02-05 16:30:17 +0530

    made changes related to styling, signout etc

--- Frontend/src/Components/Navbar.svelte
+++ Frontend/src/Components/Navbar.svelte
@@ -1,5 +1,27 @@
-<nav class="navbar navbar-expand-lg navbar-light ">
-    <a class="navbar-brand" href="#">Your Company</a>
+<script>
+    import RegisterEmployeeModal from "./RegisterEmployeeModal.svelte";
+    import { user } from "../stores/userStore";
+    let showModal = false;
+    
+
+    const openModal=()=> {
+        showModal = true;
+    }
+
+    const handleSignout=()=>{
+        // localStorage.removeItem('jwt');  
+        // user.set(null)
+    }
+</script>
+
+
+{#if showModal}
+    <RegisterEmployeeModal on:modalClosed={()=>showModal=false} />
+{/if}
+
+
+<nav class="navbar navbar-expand-lg " style="border:1px blue;box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.05); padding-left:2%; padding-right:2%"  >
+    <a class="navbar-brand" href="#">Employee Leave Management System</a>
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -8,10 +30,10 @@
     <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
         <ul class="nav navbar-nav ml-auto" >
             <li class="nav-item">
-                <a class="nav-link" href="#">Create Employee</a>
+                <a class="nav-link" href="#" on:click|preventDefault={openModal}>Create Employee</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Sign Out</a>
+                <a class="nav-link" href="#" on:click|preventDefault={handleSignout}>Sign Out</a>
             </li>
         </ul>
     </div>
