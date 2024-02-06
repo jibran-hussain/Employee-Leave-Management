cb1d5842fa4f0600cb22f25ea005cbf39ebacd0a
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-02-06 17:13:19 +0530

    imported global.css

--- Frontend/src/routes/+layout.svelte
+++ Frontend/src/routes/+layout.svelte
@@ -2,6 +2,7 @@
     import {goto} from '$app/navigation'
     import { onMount } from 'svelte';
     import { user } from '../stores/userStore';
+    import '../global.css'
 
     onMount(()=>{
         if(!$user) goto('/')
