e8621297dfa6e1b53d06e164644f7cf3a80c7ff1
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-02-06 15:35:32 +0530

    fixed user store logic related to authentication

--- Frontend/src/stores/userStore.js
+++ Frontend/src/stores/userStore.js
@@ -1,3 +1,11 @@
 import {writable} from 'svelte/store';
+import {decodeJwtToken} from '../utils/decodeJwtToken.js'
+import { browser } from '$app/environment';
 
-export const user=writable({})
+const authStateFromLocalStorage = browser && window.localStorage.getItem('jwt')
+const initialAuthState = authStateFromLocalStorage ? decodeJwtToken(JSON.parse(authStateFromLocalStorage)) : null;
+
+console.log(`The value in user store currently is :`,initialAuthState)
+
+
+export const user=writable(initialAuthState)
