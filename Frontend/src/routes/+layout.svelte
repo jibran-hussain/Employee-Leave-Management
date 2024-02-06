61ccd3a0cdf688c1e228e9ec4066892f72096fd8
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-02-06 15:41:58 +0530

    introduced this file to check if jwt token is not in localstorage, then redirect to signin

---
+++ Frontend/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import {goto} from '$app/navigation'
+    import { onMount } from 'svelte';
+    import { user } from '../stores/userStore';
+
+    onMount(()=>{
+        if(!$user) goto('/')
+    })
+</script>
+
+<slot />
