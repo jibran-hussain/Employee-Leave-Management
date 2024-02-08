d63104b9509636ffa4a7bd861f3aee256453b586
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-02-08 21:50:34 +0530

    formatted the code

--- Frontend/src/Components/Navbar.svelte
+++ Frontend/src/Components/Navbar.svelte
@@ -13,7 +13,6 @@
     const handleSignout=()=>{
         localStorage.removeItem('jwt');  
         goto('/')
-        // user.set(null)
         
     }
 </script>
@@ -23,27 +22,6 @@
     <RegisterEmployeeModal on:modalClosed={()=>showModal=false} />
 {/if}
 
-
-<!-- <nav class="navbar navbar-expand-lg navbar-light" style="border:1px blue;box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.05); padding-left:2%; padding-right:2%">
-    <a class="navbar-brand" href="#">Employee Leave Management System</a>
-
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-        <ul class="navbar-nav ml-auto">
-            {#if $user.role != 'employee'}
-            <li class="nav-item">
-                <a class="nav-link" href="#" on:click|preventDefault={openModal}>Register Employee</a>
-            </li>
-            {/if}
-            <li class="nav-item">
-                <a class="nav-link" href="#" on:click|preventDefault={handleSignout}>Sign Out</a>
-            </li>
-        </ul>
-    </div>
-</nav> -->
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
