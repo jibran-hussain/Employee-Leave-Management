3d3d155aff86d1ec8720552997a7e8328af43296
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-02-18 12:03:17 +0530

    deleted this route

--- Frontend/src/routes/dashboard/employees/search/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import EmployeeProfile from "../../../../views/EmployeeProfile.svelte";
-    import { user } from "../../../../stores/userStore";
-</script>
-
-{#if $user && ($user.role === 'admin' || $user.role === 'superadmin')}
-    <EmployeeProfile />
-{/if}
