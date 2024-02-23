3236c702c1bdebcdb29e6a4494f19e376121637f
Jibran <jibran.hussain.mir@indxtechnology.com>
2024-02-23 23:00:02 +0530

    fixed issue related to changing of schema of database

--- Backend/db/connection.js
+++ Backend/db/connection.js
@@ -5,11 +5,6 @@ import Leave from '../src/models/leaves.js';
 export const connectToDB=async(req,res,next)=>{
     try{
         await sequelize.authenticate();
-        // has to many association
-        Employee.hasMany(Leave,{foreignKey:"employeeId"})
-        Leave.belongsTo(Employee,{foreignKey:"employeeId"})
-        await Employee.sync();
-        await Leave.sync();
         console.log('Connection has been established successfully.');
         next()
       } catch (error) {
